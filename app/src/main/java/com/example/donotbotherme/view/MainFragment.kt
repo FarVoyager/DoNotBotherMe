34abe3f97a63955cd3be1b61ac50cabcef0922a1
Oleg <wowgameoleg@mail.ru>
2021-08-02 17:10:43 +0300

    1st version of UI implemented

--- app/src/main/java/com/example/donotbotherme/view/MainFragment.kt
+++ app/src/main/java/com/example/donotbotherme/view/MainFragment.kt
@@ -5,19 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import com.example.donotbotherme.R
+import com.example.donotbotherme.databinding.FragmentMainBinding
+
 
 class MainFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.createBtn.setOnClickListener {
+            requireActivity().supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, ConditionFragment.newInstance())
+                .commit()
+        }
+
     }
 
     companion object {
